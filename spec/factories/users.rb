21e4852d4b691e9f49b3ff96e94b1a791c60525e
Ryan Condron <rebelwebdevelopment@gmail.com>
2017-10-24 22:50:24 -0500

    setup user to be a devise model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :user do
     first_name 'John'
     last_name 'Doe'
+    email 'jdoe@site.local'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe User, type: :model do
 
   it { should have_db_column :first_name }
   it { should have_db_column :last_name }
+  it { should have_db_column :email }
+  it { should have_db_column :encrypted_password }
+  it { should have_db_column :reset_password_token }
+  it { should have_db_column :reset_password_sent_at }
+  it { should have_db_column :remember_created_at }
+  it { should have_db_column :sign_in_count }
+  it { should have_db_column :current_sign_in_at }
+  it { should have_db_column :last_sign_in_at }
+  it { should have_db_column :current_sign_in_ip }
+  it { should have_db_column :last_sign_in_ip }
 
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
