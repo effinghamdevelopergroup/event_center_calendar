cc731af1295bf4520d11cf78434132b3581dc037
Ryan Condron <rebelwebdevelopment@gmail.com>
2017-10-24 23:04:29 -0500

    Setup devise for Authentication (#8)
    Setup devise for user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# model to authenitcate for the application
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
